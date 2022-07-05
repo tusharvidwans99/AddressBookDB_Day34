f406fe7429207ff2deb5ab361df747fe4a6b7f65
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-06 01:24:01 +0530

    [Tushar Vidwans] Updating the data in Address Book Service database by their firstname, lastname & AddressBookName

--- AddressBook_ADO.NET/AddressBookContactDetails.cs
+++ AddressBook_ADO.NET/AddressBookContactDetails.cs
@@ -23,5 +23,23 @@ namespace AddressBook_ADO.NET
         public int typeId { get; set; }
         public string typeName { get; set; }
 
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is AddressBookContactDetails))
+            {
+                return false;
+            }
+            return ((this.firstName == (((AddressBookContactDetails)obj).firstName)) && (this.lastName == (((AddressBookContactDetails)obj).lastName)) && (this.address == (((AddressBookContactDetails)obj).address)) && (this.city == (((AddressBookContactDetails)obj).city)) && (this.state == (((AddressBookContactDetails)obj).state)) && (this.zip == (((AddressBookContactDetails)obj).zip)) && (this.phoneNo == (((AddressBookContactDetails)obj).phoneNo)) && (this.eMail == (((AddressBookContactDetails)obj).eMail)) && (this.addressBookName == (((AddressBookContactDetails)obj).addressBookName)));
+        }
+        public override int GetHashCode()
+        {
+            return this.firstName.GetHashCode() ^ this.lastName.GetHashCode() ^ this.address.GetHashCode() ^ this.city.GetHashCode() ^ this.state.GetHashCode() ^ this.zip.GetHashCode() ^ this.phoneNo.GetHashCode() ^ this.eMail.GetHashCode() ^ this.addressBookName.GetHashCode();
+        }
+
     }
 }
