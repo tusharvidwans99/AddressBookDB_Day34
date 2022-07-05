98ef2f86d53efbacfe418867b2405b1237599b14
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-05 22:57:58 +0530

    [Tushar Vidwans] Adding Welcome message

---
+++ AddressBook_ADO.NET/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBook_ADO.NET
+{
+    /// <summary>
+    /// In this program we are going to manage Address Book using DataBase.
+    /// </summary>
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the AddressBook DataBase problem");
+        }
+    }
+}
